305bbdeac043cd687a15502bb579970e24d0e147
Pablo Galán Sabugo <70710895+pablogalan1981@users.noreply.github.com>
2020-09-23 20:24:42 -0500

    Update Get-MWMigrationProjectStatistics.ps1

--- Get-MWMigrationProjectStatistics.ps1
+++ Get-MWMigrationProjectStatistics.ps1
@@ -5,9 +5,9 @@
 .DESCRIPTION
     The script exports to CSV files the MigrationWiz project statistics and the project error list of a selected project or of all projects under a Customer.
     This script is menu-guided but optionally accepts parameters to skip all menu selections: 
-    -BitTitanWorkgroupID
-    -BitTitanCustomerID
-    -BitTitanProjectID
+    -BitTitanWorkgroupId
+    -BitTitanCustomerId
+    -BitTitanProjectId
     -BitTitanProjectType ('Mailbox','Archive','Storage','PublicFolder','Teamwork')
     
 .NOTES

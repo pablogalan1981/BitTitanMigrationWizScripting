6d5322a17352d82ace603c213ede12071a19ad52
Pablo Galán Sabugo <70710895+pablogalan1981@users.noreply.github.com>
2020-11-05 12:23:18 -0600

    Update Start-MWMigrationsFromCSVFile.ps1

--- Start-MWMigrationsFromCSVFile.ps1
+++ Start-MWMigrationsFromCSVFile.ps1
@@ -1,4 +1,14 @@
 <#
+Copyright 2020 BitTitan, Inc.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+#>
+
+<#
 .SYNOPSIS
     Script to start migrations.
     
@@ -2282,7 +2292,7 @@ Function Menu-MigrationSubmission() {
                     elseif ($projectType -eq "Mailbox" -and $projectName -notmatch "All conversations") {
                         if ($preStage) {
                             if (-not [string]::IsNullOrEmpty($mailbox.ImportEmailAddress)) {
-                                $msg = "WARNING: $submittedCount/$migrationsToSubmitCount '$migrationType' with '$itemTypes' pre-stage pass with date filtering '$itemEndDate' for '$($mailbox.ImportEmailAddress)'."
+                                $msg = "WARNING: $submittedCount/$migrationsToSubmitCount '$migrationType' with '$itemTypes' pre-stage pass with end date filtering '$itemEndDate' for '$($mailbox.ImportEmailAddress)'."
                             }
 
                             $result = Add-MW_MailboxMigration -Ticket $script:mwticket -MailboxId $mailbox.MailboxId -ConnectorId $mailbox.ConnectorId -Type $migrationType -UserId $script:mwticket.UserId -Priority 1 -ItemTypes $itemTypes -Status Submitted -ItemEndDate $itemEndDate -errorAction Stop
